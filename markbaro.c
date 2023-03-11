4f405392d810cdcb7b6144f21120b22646a7e71a
Markbaro <markbarasa2021@gmail.com>
2023-03-10 23:39:47 -0800

    c low level programming

---
+++ markbaro.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/***PROGRAMMER MARKBARASA***/
+
+int main()
+{
+    	// Personal Information
+    	char name[] = "Mark Barasa";
+    	char email[] = "markbarasa2021@gmail.com";
+
+    	// Skills
+    	char skills[] = "C Programming, Linux Shell Bash, System Documentation, Algorithms, Pseudocoding, Flow Charts";
+
+    	// Learning
+   	char learning[] = "Advanced C Programming, Data Structures";
+
+    	// Interests
+    	char interests[] = "Collaborating on C coding challenges and simple programs, attending software engineering events, bootcamps, conferences and hackathons";
+
+    	// Fun Fact
+    	char funFact[] = "I love to explore different programming languages and technologies, and I believe in the power of open-source software.";
+
+    	printf("Name: %s\n", name);
+    	printf("Email: %s\n", email);
+    	printf("Skills: %s\n", skills);
+    	printf("Learning: %s\n", learning);
+	printf("Interests: %s\n", interests);
+	printf("Fun Fact: %s\n", funFact);
+
+    	return 0;
+}
+
