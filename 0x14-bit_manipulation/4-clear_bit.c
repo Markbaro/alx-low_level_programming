ead6169cd90963c7359bbd65a7a20688f120cbc8
Markbaro <122884513+Markbaro@users.noreply.github.com>
2023-04-09 23:55:21 +0300

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -13,12 +13,9 @@ int clear_bit(unsigned long int *n, unsigned int index)
 
 	if (index >= sizeof(unsigned long int) * 8)
 	{
-		return (-1);
-	}
-	else
-	{
-		x = (1UL << index);
-		*n &= x;
+		*n &= (~(1 << index));
 		return (1);
 	}
+	return (-1);
 }
+
