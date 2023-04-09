86516af80e7ef669cf792ea4c858c62e69c61c1e
Markbaro <markbarasa2021@gmail.com>
2023-04-09 09:38:09 -0700

    copies the content of a file to another file.

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,94 @@
+#include "main.h"
+#include "stdlib.h"
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+#define BUF_SIZE 1024
+
+
+
+/**
+ *print_error - print error messages
+ *@error - specific error message
+ *Return : 1 on success 0r -1 if it fails
+ *
+ */
+
+void print_error(int error, char *arg)
+{
+	switch (error)
+	{
+		case 97:
+			dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", arg);
+			break;
+		case 98:
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", arg);
+			break;
+		case 99:
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", arg);
+			break;
+		case 100:
+			dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", arg);
+			break;
+		default:
+			break;
+	}
+}
+
+/**
+ * main - copies contents from one file to onother
+ * @argc: number of arguments
+ * @argv: pointer to arguments
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int fp1, fp2, readit, writeit;
+	char buf[BUF_SIZE];
+	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+
+	if (argc != 3)
+	{
+		print_error(97, argv[0]);
+		exit(97);
+	}
+	fp1 = open(argv[1], O_RDONLY);
+	if (fp1 == -1)
+	{
+		print_error(98, argv[1]);
+		exit(98);
+	}
+	fp2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
+	if (fp2 == -1)
+	{
+		print_error(99, argv[2]);
+		exit(99);
+	}
+	while ((readit = read(fp1, buf, BUF_SIZE)) > 0)
+	{
+		writeit = write(fp2, buf, readit);
+		if (writeit != readit)
+		{
+			print_error(99, argv[2]);
+			exit(99);
+		}
+	}
+	if (readit == -1)
+	{
+		print_error(98, argv[1]);
+		exit(98);
+	}
+	if (close(fp1) == -1)
+	{
+		print_error(100, strerror(errno));
+		exit(100);
+	}
+	if (close(fp2) == -1)
+	{
+		print_error(100, strerror(errno));
+		exit(100);
+	}
+	return (0);
+}
