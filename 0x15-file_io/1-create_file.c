a74ab7d212870d94564cbd64d491c8bd85c09082
Markbaro <markbarasa2021@gmail.com>
2023-04-10 03:38:15 -0700

    create file & add permission

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,9 +3,9 @@
 
 /**
  * create_file - creates file and adds permisssion
- * @filename - file to be created
- * @text_content - content to be written
- * Return : 1 or -1 if it fails
+ * @filename : file to be created
+ * @text_content : content to be written
+ * Return: 1 or -1 if it fails
  */
 
 int create_file(const char *filename, char *text_content)
