15c16f6008350d88a34d829c03840c1207744c64
Markbaro <markbarasa2021@gmail.com>
2023-04-10 03:44:45 -0700

    Append text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 
 /**
- *append_text_to_file - appends text to a file
- *@filename - file to append text to
- *@text_content - text to be appended
- *Return : 1 0r -1 if it fails
+ * append_text_to_file - appends text to a file
+ * @filename : file to append text to
+ * @text_content : text to be appended
+ * Return: 1 0r -1 if it fails
  */
 
 int append_text_to_file(const char *filename, char *text_content)
