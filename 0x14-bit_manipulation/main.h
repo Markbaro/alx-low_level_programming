5110dd92a509d609fbcb65055e83c0a627437849
Markbaro <markbarasa2021@gmail.com>
2023-05-04 10:49:00 -0700

    returns the value of a bit at a given index

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,8 +2,10 @@
 #define HOLBERTON_H
 
 #include <stddef.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdio.h>
 
-int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
